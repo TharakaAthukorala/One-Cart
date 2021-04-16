fb4406bd9ca5860521f30a835382a4f153937657
Tharaka Athukorala <52407424+TharakaAthukorala@users.noreply.github.com>
2021-04-16 19:25:54 +0530

    Add files via upload

---
+++ chat_system/header.php
@@ -0,0 +1,17 @@
+<div id="header">
+    <div id="logo">
+        <a href="index1.php?home"><img src="../images/onecart white.png" style="width:60%;" alt=""/></a>
+    </div>
+    <div id="name">
+    <h1>ONE CART SHOPPING</h1>
+    </div>
+
+    <div id="link">
+    <ul>
+        <li>
+            <a href="../index.php"><b>Logout</b></a>
+        </li>
+    </ul>
+    </div>
+
+</div>
